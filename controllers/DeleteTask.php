9553b2433457ff3924374ad516782b06ca62c283
ANSANDRIA <fjsimarmata96@gmail.com>
2026-06-18 15:53:18 +0700

    Selesai membuat task

---
+++ controllers/DeleteTask.php
@@ -0,0 +1,12 @@
+<?php
+
+// tangkap id_laporan //1
+// validasi, apakah id_laporan ada??
+// jika tidak -error response 
+// jika ya - hapus data pada table 
+
+// konsep hapus
+// 1. hard delete -> hapus permanen, dan umumnya tidak diperbolehkan untuk data penting
+// 2. soft delete -> tidak hapus, melainkan disembunyikan (show/hide pada table)
+
+?>
