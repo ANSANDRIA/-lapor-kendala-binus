80fbcea057ed5df927bd5c9e4f64ec271e71ddbf
ANSANDRIA <fjsimarmata96@gmail.com>
2026-06-18 15:10:54 +0700

    add helpers, add login and register api

--- koneksi.php
+++ koneksi.php
@@ -5,13 +5,13 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $host = "localhost";
-$database = "db_todo";
+$database = "db_lapor_kendala";
 $username = "root";
 $password = "";
 
 // membuat koneksi database
 $koneksi = mysqli_connect($host, $username, $password, $database);
-
+$jwt_secret = "20c9f605925dacfeb0b16a252b39019094cbc1a20cd53dbced4144308aa0d399";
 if (!$koneksi) {
     header("Content-Type: application/json");
     http_response_code(500);
